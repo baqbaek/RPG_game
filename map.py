ea662d521577fbb9f336c8a9c41d39c260a6eccc
Bartek <baqbaek@gmail.com>
2023-02-04 20:13:37 +0100

    adding map and enemies

--- map.py
+++ map.py
@@ -1,10 +1,25 @@
 import numpy as np
+import random as rand
 
 matrix = np.zeros((10, 20), dtype=str)
-matrix[:, :] = '--'
+matrix[:, :] = '-'
 matrix[1:-1, :] = '|'
-matrix[1:-1, 1:-1] = 'x'
+matrix[1:-1, 1:-1] = ' '
 
-for row in matrix:
-    print(''.join(row))
+for i in range(0, 5):
+    row = rand.randint(1, 8)
+    col = rand.randint(1, 18)
+    matrix[row][col] = 'E'
 
+print(' ', end='  ')
+for i in range(0, 20):
+    if i == 19:
+        print(chr(65 + i))
+    else:
+        print(chr(65 + i), end=' ')
+
+x = 0
+for i in matrix:
+    print(x, end='  ')
+    x += 1
+    print(' '.join(i))
