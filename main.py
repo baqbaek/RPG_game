42705e47c7b0d4a45358d8eb4139163e271fdf64
Bartek <baqbaek@gmail.com>
2023-02-05 16:34:08 +0100

    gameplay update

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import soldier
-import map
-import enemies
+from soldier import *
+from map import *
+from enemies import *
 
-print('hi')
+print('hi (About game)')
+# selected_soldier = Soldier.choose_soldier(soldiers)
+enemy_spawn(int(input('Number of enemies: ')))
+player_spawn()
+display_map()
+move_player(input('Direction: '))
 
+# selected_soldier.actions()
